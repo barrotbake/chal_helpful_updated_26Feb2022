f454a4ace91972a4a2dd5172eb37b7c41bb735b8
barrotbake <nguyenjacob411@gmail.com>
2022-03-02 13:53:15 -0800

    alan_jacob repo

---
+++ challenge_4.h
@@ -0,0 +1,6 @@
+#ifndef __CHALLENGE_4_H__
+#define __CHALLENGE_4_H__
+
+int challenge_4();
+
+#endif 
---
+++ challenge_5.h
@@ -0,0 +1,6 @@
+#ifndef __CHALLENGE_5_H__
+#define __CHALLENGE_5_H__
+
+int challenge_5();
+
+#endif 
